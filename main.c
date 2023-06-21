747014255d04a89b7a67460d769eb30baacf480e
Mohamed Khalifa <120184831+Mohamed-Khalifa12@users.noreply.github.com>
2023-06-21 05:48:43 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,5 @@
 #include "monty.h"
-
 bus_t bus = {NULL, NULL, NULL, 0};
-
 /**
 * main - monty code interpreter
 * @argc: number of arguments
@@ -43,5 +41,5 @@ int main(int argc, char *argv[])
 	}
 	free_stack(stack);
 	fclose(file);
-    return (0);
+return (0);
 }
