cdf7770ce7ce185cca8dcdf98ea303ce639aa04a
Mohamed Khalifa <120184831+Mohamed-Khalifa12@users.noreply.github.com>
2023-06-21 05:46:25 +0300

    Update push.c

--- push.c
+++ push.c
@@ -5,7 +5,7 @@
  * @counter: line_number
  * Return: no return
 */
-void push(stack_t **head, unsigned int counter)
+void f_push(stack_t **head, unsigned int counter)
 {
 	int n, j = 0, flag = 0;
 
@@ -16,29 +16,22 @@ void push(stack_t **head, unsigned int counter)
 		for (; bus.arg[j] != '\0'; j++)
 		{
 			if (bus.arg[j] > 57 || bus.arg[j] < 48)
-				flag = 1; 
-        }
+				flag = 1; }
 		if (flag == 1)
-		{ 
-            fprintf(stderr, "L%d: usage: push integer\n", counter);
+		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
 			fclose(bus.file);
 			free(bus.content);
 			free_stack(*head);
-			exit(EXIT_FAILURE); 
-        }
-    }
+			exit(EXIT_FAILURE); }}
 	else
-	{ 
-        fprintf(stderr, "L%d: usage: push integer\n", counter);
+	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
-		exit(EXIT_FAILURE); 
-    }
-    
+		exit(EXIT_FAILURE); }
 	n = atoi(bus.arg);
-	if (bus.type == 0)
-		addstackNode(head, n);
+	if (bus.lifi == 0)
+		addnode(head, n);
 	else
-		addqueueNode(head, n);
+		addqueue(head, n);
 }
